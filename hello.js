7b5f10f0d724a68646b7e984562cfc0763a63ad0
sunsetbb <andyyuenouyang@gmail.com>
2022-01-18 01:07:56 +0000

    javascript added

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener("click",function(g){
+        g.preventDefault();             //this is used because default jumps to the page without animation
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
